429e6bec86a727b4668fe38983f06c51851c3cb2
aspeiro3@gmail.com <aspeiro3@gmail.com>
2020-07-01 17:45:14 +0200

    user authorization immediately after registration

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,7 +16,8 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      redirect_to root_url, notice: 'Пользователь успешно зарегестрирован!'
+      session[:user_id] = @user.id
+      redirect_to user_path(@user), notice: 'Вы успешно зарегестрировались!'
     else
       render 'new'
     end
